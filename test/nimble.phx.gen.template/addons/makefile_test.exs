b3bcbf606a8d4131c7d8ec2b9b9a061057c38d11
An Duong <andyduong1920@gmail.com>
2020-09-30 17:17:33 +0700

    fix test new app

--- test/nimble.phx.gen.template/addons/makefile_test.exs
+++ test/nimble.phx.gen.template/addons/makefile_test.exs
@@ -2,7 +2,7 @@ defmodule Nimble.Phx.Gen.Template.Addons.MakefileTest do
   use Nimble.Phx.Gen.Template.AddonCase, async: true
 
   describe "#apply/2" do
-    test "copies the Makefile into the project", %{project: project, test_app_path: test_app_path} do
+    test "copies the Makefile", %{project: project, test_app_path: test_app_path} do
       in_test_app(test_app_path, fn ->
         Addons.Makefile.apply(project, {})
 
