f057783112b514005d87862223216d09838c1666
An Duong <andyduong1920@gmail.com>
2020-09-30 17:03:42 +0700

    adding assert_file file

--- test/nimble.phx.gen.template/addons/makefile_test.exs
+++ test/nimble.phx.gen.template/addons/makefile_test.exs
@@ -6,7 +6,11 @@ defmodule Nimble.Phx.Gen.Template.Addons.MakefileTest do
       in_test_app(test_app_path, fn ->
         Addons.Makefile.apply(project, {})
 
-        assert_file("Makefile")
+        assert_file("Makefile", fn file ->
+          assert file =~ ".PHONY: docker_setup"
+          assert file =~ "docker_setup:"
+          assert file =~ "docker-compose -f docker-compose.dev.yml up -d"
+        end)
       end)
     end
   end
--- test/support/addon_case.ex
+++ test/support/addon_case.ex
@@ -16,6 +16,23 @@ defmodule Nimble.Phx.Gen.Template.AddonCase do
       defp assert_file(file) do
         assert File.regular?(file), "Expected #{file} to exist, but does not"
       end
+
+      defp assert_file(file, match) do
+        cond do
+          is_list(match) ->
+            assert_file(file, &Enum.each(match, fn m -> assert &1 =~ m end))
+
+          is_binary(match) or Regex.regex?(match) ->
+            assert_file(file, &assert(&1 =~ match))
+
+          is_function(match, 1) ->
+            assert_file(file)
+            match.(File.read!(file))
+
+          true ->
+            raise inspect({file, match})
+        end
+      end
     end
   end
 
