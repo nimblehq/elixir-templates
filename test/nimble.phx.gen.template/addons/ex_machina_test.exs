8868ab2729b556faed43c253996b03c41bb6405c
An Duong <andyduong1920@gmail.com>
2020-10-05 11:43:39 +0700

    adding ex_machina_test

---
+++ test/nimble.phx.gen.template/addons/ex_machina_test.exs
@@ -0,0 +1,65 @@
+defmodule Nimble.Phx.Gen.Template.Addons.ExMachinaTest do
+  use Nimble.Phx.Gen.Template.AddonCase
+
+  describe "#apply/2" do
+    test "copies the test/support/factory.ex", %{
+      project: project,
+      test_project_path: test_project_path
+    } do
+      in_test_project(test_project_path, fn ->
+        Addons.ExMachina.apply(project, {})
+
+        assert_file("test/support/factory.ex")
+      end)
+    end
+
+    test "injects ex_machina to mix dependency", %{
+      project: project,
+      test_project_path: test_project_path
+    } do
+      in_test_project(test_project_path, fn ->
+        Addons.ExMachina.apply(project, {})
+
+        assert_file("mix.exs", fn file ->
+          assert file =~ """
+                   defp deps do
+                     [
+                       {:ex_machina, \"~> 2.4\", [only: :test]},
+                 """
+        end)
+      end)
+    end
+
+    test "updates test/test_helper.exs", %{project: project, test_project_path: test_project_path} do
+      in_test_project(test_project_path, fn ->
+        Addons.ExMachina.apply(project, {})
+
+        assert_file("test/test_helper.exs", fn file ->
+          assert file =~ """
+                 {:ok, _} = Application.ensure_all_started(:ex_machina)
+
+                 ExUnit.start()
+                 """
+        end)
+      end)
+    end
+
+    test "adds Factory module", %{project: project, test_project_path: test_project_path} do
+      in_test_project(test_project_path, fn ->
+        Addons.ExMachina.apply(project, {})
+
+        assert_file("test/support/data_case.ex", fn file ->
+          assert file =~ "import NimblePhxGenTemplate.Factory"
+        end)
+
+        assert_file("test/support/channel_case.ex", fn file ->
+          assert file =~ "import NimblePhxGenTemplate.Factory"
+        end)
+
+        assert_file("test/support/conn_case.ex", fn file ->
+          assert file =~ "import NimblePhxGenTemplate.Factory"
+        end)
+      end)
+    end
+  end
+end
--- test/nimble.phx.gen.template/addons/makefile_test.exs
+++ test/nimble.phx.gen.template/addons/makefile_test.exs
@@ -6,14 +6,7 @@ defmodule Nimble.Phx.Gen.Template.Addons.MakefileTest do
       in_test_project(test_project_path, fn ->
         Addons.Makefile.apply(project, {})
 
-        assert_file("Makefile", fn file ->
-          assert file =~ """
-                 .PHONY: docker_setup
-
-                 docker_setup:
-                 \tdocker-compose -f docker-compose.dev.yml up -d
-                 """
-        end)
+        assert_file("Makefile")
       end)
     end
   end
