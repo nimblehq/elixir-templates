f4731fa6b3f97b97bd1860968a53d2ea69a4318d
An Duong <andyduong1920@gmail.com>
2020-10-05 15:23:33 +0700

    fetch latest package version

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,11 @@ use Mix.Config
 
 config :phoenix, :json_library, Jason
 
+config :nimble_phx_gen_template,
+  http_adapter: Nimble.Phx.Gen.Template.HttpClient.HttpAdapter,
+  hex_client: Nimble.Phx.Gen.Template.Hex.HexClient,
+  hex_package: Nimble.Phx.Gen.Template.Hex.Package
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
