5aaceda0e3222bfdc37c71943b4a1d099fb40670
An Duong <andyduong1920@gmail.com>
2020-11-18 16:35:51 +0700

    Update the max line_length in credo

--- priv/templates/nimble.phx.gen.template/.credo.exs
+++ priv/templates/nimble.phx.gen.template/.credo.exs
@@ -96,7 +96,7 @@
         {Credo.Check.Readability.AliasOrder, []},
         {Credo.Check.Readability.FunctionNames, []},
         {Credo.Check.Readability.LargeNumbers, []},
-        {Credo.Check.Readability.MaxLineLength, [priority: :low, max_length: 120]},
+        {Credo.Check.Readability.MaxLineLength, [priority: :low, max_length: 100]},
         {Credo.Check.Readability.ModuleAttributeNames, []},
         {Credo.Check.Readability.ModuleDoc, false},
         {Credo.Check.Readability.ModuleNames, []},
