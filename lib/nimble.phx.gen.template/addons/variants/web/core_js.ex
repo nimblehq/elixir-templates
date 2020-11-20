c820d322a4c1d6791ae4dabd07befcb3ddef1b6d
An Duong <andyduong1920@gmail.com>
2020-11-20 16:56:45 +0700

    Adding CoreJS

---
+++ lib/nimble.phx.gen.template/addons/variants/web/core_js.ex
@@ -0,0 +1,48 @@
+defmodule Nimble.Phx.Gen.Template.Addons.Web.CoreJS do
+  use Nimble.Phx.Gen.Template.Addon
+
+  @impl true
+  def do_apply(%Project{} = project, _opts) do
+    edit_files(project)
+  end
+
+  defp edit_files(%Project{} = project) do
+    project
+    |> edit_package_json()
+    |> edit_app_js()
+
+    project
+  end
+
+  def edit_package_json(project) do
+    Generator.replace_content(
+      "assets/package.json",
+      """
+          "phoenix_html": "file:../deps/phoenix_html"
+      """,
+      """
+          "phoenix_html": "file:../deps/phoenix_html",
+          "core-js": "^3.7.0"
+      """
+    )
+
+    project
+  end
+
+  def edit_app_js(project) do
+    Generator.replace_content(
+      "assets/js/app.js",
+      """
+      import "phoenix_html"
+      """,
+      """
+      import "core-js/stable"
+      import "regenerator-runtime/runtime"
+
+      import "phoenix_html"
+      """
+    )
+
+    project
+  end
+end
