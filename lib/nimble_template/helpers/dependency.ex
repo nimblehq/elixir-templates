065caef69734567d6c97870e791e8f6ba9a041d0
Miicky Jittjana <immicky.a@gmail.com>
2021-09-17 12:02:15 +0700

    Order dependencies

--- lib/nimble_template/helpers/dependency.ex
+++ lib/nimble_template/helpers/dependency.ex
@@ -1,5 +1,34 @@
 defmodule NimbleTemplate.DependencyHelper do
-  def fetch_and_install_dependencies(),
+  alias NimbleTemplate.Generator
+
+  def fetch_and_install_dependencies,
     do:
       if(Mix.shell().yes?("\nFetch and install dependencies?"), do: Mix.shell().cmd("mix deps.get"))
+
+  def order_dependencies! do
+    file_content = File.read!("mix.exs")
+
+    dependencies = extract_dependencies(file_content)
+
+    ordered_dependencies =
+      dependencies
+      |> String.split(",\n")
+      |> Enum.sort()
+      |> Enum.join(",\n")
+
+    Generator.replace_content("mix.exs", dependencies, ordered_dependencies)
+  end
+
+  def extract_dependencies(contents) do
+    [_, deps_with_tail] =
+      :binary.split(contents, """
+        defp deps do
+          [
+      """)
+
+    [deps, _tail] =
+      :binary.split(deps_with_tail, "\n    ]\n  end")
+
+    deps
+  end
 end
