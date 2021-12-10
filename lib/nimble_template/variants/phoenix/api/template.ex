de07d78992e73d2a8624e6a6727542e0580000a1
An Duong <andyduong1920@gmail.com>
2021-12-10 14:22:33 +0700

    Remove cache_static_manifest config

--- lib/nimble_template/variants/phoenix/api/template.ex
+++ lib/nimble_template/variants/phoenix/api/template.ex
@@ -1,9 +1,12 @@
 defmodule NimbleTemplate.Phoenix.Api.Template do
   @moduledoc false
 
+  alias NimbleTemplate.Addons.Phoenix.Api
   alias NimbleTemplate.Project
 
   def apply(%Project{} = project) do
+    Api.Config.apply(project)
+
     project
   end
 end
