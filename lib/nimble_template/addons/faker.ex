9a2c0eff70f1ebfdbee9c1413f101c3ca471efa6
Terone <bterone@gmail.com>
2021-12-21 16:22:14 +0700

    Remove square brackets in options

--- lib/nimble_template/addons/faker.ex
+++ lib/nimble_template/addons/faker.ex
@@ -6,7 +6,7 @@ defmodule NimbleTemplate.Addons.Faker do
   @impl true
   def do_apply(%Project{} = project, _opts) do
     Generator.inject_mix_dependency(
-      {:faker, latest_package_version(:faker), [only: [:dev, :test], runtime: false]}
+      {:faker, latest_package_version(:faker), only: [:dev, :test], runtime: false}
     )
 
     project
