66186e6befb936dff8af46241bf33faa979eb37e
Thanh Huynh <huynhbathanh@gmail.com>
2021-12-28 11:54:22 +0700

    Remove page_view and layout_view on API addon

--- lib/nimble_template/addons/variants/phoenix/api/error_view.ex
+++ lib/nimble_template/addons/variants/phoenix/api/error_view.ex
@@ -40,14 +40,9 @@ defmodule NimbleTemplate.Addons.Phoenix.Api.ErrorView do
 
   defp delete_files(%Project{web_test_path: web_test_path, web_path: web_path} = project) do
     files_to_delete = [
-      "#{web_path}/views/layout_view.ex",
-      "#{web_path}/views/page_view.ex",
       "#{web_path}/views/error_view.ex",
       "#{web_path}/views/error_helpers.ex",
-      "#{web_test_path}/views/layout_view_test.exs",
-      "#{web_test_path}/views/page_view_test.exs",
-      "#{web_test_path}/views/error_view_test.exs",
-      "#{web_test_path}/controllers/page_controller_test.exs"
+      "#{web_test_path}/views/error_view_test.exs"
     ]
 
     Enum.each(files_to_delete, fn path ->
--- test/nimble_template/addons/variants/phoenix/api/error_view_test.exs
+++ test/nimble_template/addons/variants/phoenix/api/error_view_test.exs
@@ -14,12 +14,6 @@ defmodule NimbleTemplate.Addons.Phoenix.Api.ErrorViewTest do
         assert_file("test/nimble_template_web/views/error_helpers_test.exs")
         assert_file("test/nimble_template_web/views/error_view_test.exs")
         assert_file("test/support/view_case.ex")
-
-        refute_file("lib/nimble_template_web/views/layout_view.ex")
-        refute_file("lib/nimble_template_web/views/page_view.ex")
-        refute_file("lib/nimble_template_web/views/layout_view_test.exs")
-        refute_file("lib/nimble_template_web/views/page_view_test.exs")
-        refute_file("lib/nimble_template_web/controllers/page_controller_test.exs")
       end)
     end
   end
