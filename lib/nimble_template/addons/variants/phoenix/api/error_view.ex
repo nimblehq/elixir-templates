c77979edb01edc3bf52fbd2bd46bad2e6e5c9d1a
Thanh Huynh <huynhbathanh@gmail.com>
2021-12-28 09:19:20 +0700

    Update format

--- lib/nimble_template/addons/variants/phoenix/api/error_view.ex
+++ lib/nimble_template/addons/variants/phoenix/api/error_view.ex
@@ -26,8 +26,10 @@ defmodule NimbleTemplate.Addons.Phoenix.Api.ErrorView do
     files = [
       {:eex, "lib/otp_app_web/views/error_helpers.ex", "#{web_path}/views/error_helpers.ex"},
       {:eex, "lib/otp_app_web/views/error_view.ex", "#{web_path}/views/error_view.ex"},
-      {:eex, "test/otp_app_web/views/error_helpers_test.exs", "#{web_test_path}/views/error_helpers_test.exs"},
-      {:eex, "test/otp_app_web/views/error_view_test.exs", "#{web_test_path}/views/error_view_test.exs"},
+      {:eex, "test/otp_app_web/views/error_helpers_test.exs",
+       "#{web_test_path}/views/error_helpers_test.exs"},
+      {:eex, "test/otp_app_web/views/error_view_test.exs",
+       "#{web_test_path}/views/error_view_test.exs"},
       {:eex, "test/support/view_case.ex.eex", "test/support/view_case.ex"}
     ]
 
@@ -36,7 +38,7 @@ defmodule NimbleTemplate.Addons.Phoenix.Api.ErrorView do
     project
   end
 
-  defp delete_files(%Project{ web_test_path: web_test_path, web_path: web_path } = project) do
+  defp delete_files(%Project{web_test_path: web_test_path, web_path: web_path} = project) do
     File.rm("#{web_path}/views/layout_view.ex")
     File.rm("#{web_path}/views/page_view.ex")
     File.rm("#{web_test_path}/views/layout_view_test.exs")
