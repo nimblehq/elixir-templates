50fd3481f5d75e8fe917158a19fd0ccd1a9d0dd9
Thanh Huynh <huynhbathanh@gmail.com>
2022-01-04 10:44:01 +0700

    Assert removed files

--- lib/nimble_template/addons/variants/phoenix/api/error_view.ex
+++ lib/nimble_template/addons/variants/phoenix/api/error_view.ex
@@ -42,6 +42,8 @@ defmodule NimbleTemplate.Addons.Phoenix.Api.ErrorView do
     files_to_delete = [
       "#{web_path}/views/error_view.ex",
       "#{web_path}/views/error_helpers.ex",
+      "#{web_path}/views/layout_view.ex",
+      "#{web_path}/views/page_view.ex",
       "#{web_test_path}/views/error_view_test.exs"
     ]
 
