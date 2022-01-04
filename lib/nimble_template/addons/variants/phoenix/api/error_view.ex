d8122c127e83ca21c78e1b1a9e69fbbcc62ae729
Thanh Huynh <huynhbathanh@gmail.com>
2022-01-04 11:25:50 +0700

    Remove the controller test route in API addon

--- lib/nimble_template/addons/variants/phoenix/api/error_view.ex
+++ lib/nimble_template/addons/variants/phoenix/api/error_view.ex
@@ -44,6 +44,7 @@ defmodule NimbleTemplate.Addons.Phoenix.Api.ErrorView do
       "#{web_path}/views/error_helpers.ex",
       "#{web_path}/views/layout_view.ex",
       "#{web_path}/views/page_view.ex",
+      "#{web_test_path}/controllers/page_controller_test.exs",
       "#{web_test_path}/views/error_view_test.exs"
     ]
 
