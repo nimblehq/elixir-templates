c5e10c8286e656dedfc67b243ad57733577cc34b
An Duong <andyduong1920@gmail.com>
2022-01-04 14:46:23 +0700

    Add Data Migration

---
+++ lib/nimble_template/addons/ecto_data_migration.ex
@@ -0,0 +1,41 @@
+defmodule NimbleTemplate.Addons.EctoDataMigration do
+  @moduledoc false
+
+  use NimbleTemplate.Addon
+
+  @impl true
+  def do_apply(%Project{} = project, _opts) do
+    project
+    |> copy_files()
+    |> edit_files()
+  end
+
+  defp copy_files(%Project{} = project) do
+    Generator.copy_file([
+      {:text, "priv/repo/data_migrations/.keep", "priv/repo/data_migrations/.keep"}
+    ])
+
+    project
+  end
+
+  defp edit_files(%Project{} = project) do
+    edit_mix(project)
+
+    project
+  end
+
+  defp edit_mix(project) do
+    Generator.inject_content(
+      "mix.exs",
+      """
+        defp aliases do
+          [
+      """,
+      """
+            "ecto.migrate_all": [
+              "ecto.migrate --migrations-path=priv/repo/migrations --migrations-path=priv/repo/data_migrations"
+            ],
+      """
+    )
+  end
+end
