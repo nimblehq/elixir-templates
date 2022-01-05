a7232cc604c940449673f358e6f15dcd31a17232
An Duong <andyduong1920@gmail.com>
2022-01-05 16:25:27 +0700

    Add CSS and JS structure

--- lib/nimble_template/addons/variants/phoenix/web/nimble_css.ex
+++ lib/nimble_template/addons/variants/phoenix/web/nimble_css.ex
@@ -6,5 +6,19 @@ defmodule NimbleTemplate.Addons.Phoenix.Web.NimbleCSS do
   @impl true
   def do_apply(%Project{} = project, _opts) do
     project
+    |> remove_default_phoenix_structure()
+    |> copy_nimble_structure()
+  end
+
+  defp remove_default_phoenix_structure(project) do
+    File.rm_rf!("assets/css")
+
+    project
+  end
+
+  defp copy_nimble_structure(project) do
+    Generator.copy_directory("assets/css")
+
+    project
   end
 end
--- lib/nimble_template/addons/variants/phoenix/web/nimble_js.ex
+++ lib/nimble_template/addons/variants/phoenix/web/nimble_js.ex
@@ -6,10 +6,29 @@ defmodule NimbleTemplate.Addons.Phoenix.Web.NimbleJS do
   @impl true
   def do_apply(%Project{} = project, _opts) do
     project
+    |> copy_nimble_structure()
+    |> edit_app_js()
+  end
 
-    #     // Application
-    # import "./initializers/";
+  defp copy_nimble_structure(project) do
+    Generator.copy_directory("assets/js")
 
-    # import "./screens/";
+    project
+  end
+
+  defp edit_app_js(project) do
+    Generator.append_content(
+      "assets/js/app.js",
+      """
+
+      // Application
+      import "./initializers/";
+
+      import "./screens/";
+
+      """
+    )
+
+    project
   end
 end
