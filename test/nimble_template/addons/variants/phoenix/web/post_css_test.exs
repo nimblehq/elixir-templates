c715e105aaf6c8e24025d4194636767c0ad751fb
An Duong <andyduong1920@gmail.com>
2022-01-06 10:49:17 +0700

    Add test for PostCSS

---
+++ test/nimble_template/addons/variants/phoenix/web/post_css_test.exs
@@ -0,0 +1,34 @@
+defmodule NimbleTemplate.Addons.Phoenix.Web.PostCSSTest do
+  use NimbleTemplate.AddonCase, async: false
+
+  describe "#apply/2" do
+    test "adds autoprefixer, postcss and postcss-loader into package.json", %{
+      project: project,
+      test_project_path: test_project_path
+    } do
+      in_test_project(test_project_path, fn ->
+        AddonsWeb.PostCSS.apply(project)
+
+        assert_file("assets/package.json", fn file ->
+          assert file =~ """
+                   "devDependencies": {
+                     "autoprefixer": "^10.4.1",
+                     "postcss": "^8.4.5",
+                     "postcss-loader": "^6.2.1",
+                 """
+        end)
+      end)
+    end
+
+    test "copies the assets/postcss.config.js", %{
+      project: project,
+      test_project_path: test_project_path
+    } do
+      in_test_project(test_project_path, fn ->
+        AddonsWeb.PostCSS.apply(project)
+
+        assert_file("assets/postcss.config.js")
+      end)
+    end
+  end
+end
