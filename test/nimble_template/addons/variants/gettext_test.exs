d7018ad8ab6f40cc5d0e9202419deef1074e4ef5
Thanh Huynh <huynhbathanh@gmail.com>
2022-01-10 16:25:00 +0700

    Update assert for gettext.check

--- test/nimble_template/addons/variants/gettext_test.exs
+++ test/nimble_template/addons/variants/gettext_test.exs
@@ -19,6 +19,15 @@ defmodule NimbleTemplate.Addons.Phoenix.GettextTest do
 
         assert_file("mix.exs", fn file ->
           assert file =~ """
+                   "gettext.check": [
+                     "gettext.extract-and-merge",
+                     ~S/cmd git diff --no-ext-diff --quiet priv\\/gettext || echo "The localization files POs, POTs are NOT up-to-date."/
+                   ],
+                 """
+        end)
+
+        assert_file("mix.exs", fn file ->
+          assert file =~ """
                       "codebase.fix": [
                          "gettext.extract-and-merge",
                  """
