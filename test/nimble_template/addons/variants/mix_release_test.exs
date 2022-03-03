ee1f1480de456e151b2dbb08efe2de58d4b25b1a
An Duong <andyduong1920@gmail.com>
2022-03-03 15:24:30 +0700

    adjust the release addon

--- test/nimble_template/addons/variants/mix_release_test.exs
+++ test/nimble_template/addons/variants/mix_release_test.exs
@@ -32,5 +32,42 @@ defmodule NimbleTemplate.Addons.Phoenix.MixReleaseTest do
         end)
       end)
     end
+
+    test "adjusts the config/runtime.exs", %{
+      project: project,
+      test_project_path: test_project_path
+    } do
+      in_test_project(test_project_path, fn ->
+        PhoenixAddons.MixRelease.apply(project)
+
+        assert_file("config/runtime.exs", fn file ->
+          assert file =~ """
+                   config :nimble_template, NimbleTemplateWeb.Endpoint,
+                     server: true,
+                 """
+
+          refute file =~ """
+
+                   # ## Using releases
+                   #
+                   # If you are doing OTP releases, you need to instruct Phoenix
+                   # to start each relevant endpoint:
+                   #
+                   #     config :nimble_template, NimbleTemplateWeb.Endpoint, server: true
+                   #
+                   # Then you can assemble a release by calling `mix release`.
+                   # See `mix help release` for more information.
+                 """
+
+          refute file =~ """
+                 # Start the phoenix server if environment is set and running in a release
+                 if System.get_env("PHX_SERVER") && System.get_env("RELEASE_NAME") do
+                   config :nimble_template, NimbleTemplateWeb.Endpoint, server: true
+                 end
+
+                 """
+        end)
+      end)
+    end
   end
 end
