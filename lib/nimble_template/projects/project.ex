6b8d296b8ad37ec1f4aa93e5a60341b432d02e12
andyduong1920 <andyduong1920@users.noreply.github.com>
2022-11-09 09:43:53 +0000

    Upgrade stack versions

--- lib/nimble_template/projects/project.ex
+++ lib/nimble_template/projects/project.ex
@@ -1,10 +1,10 @@
 defmodule NimbleTemplate.Projects.Project do
   @moduledoc false
 
-  @alpine_version "3.14.6"
-  @elixir_version "1.14.0"
-  @erlang_version "25.0.4"
-  @node_asdf_version "16.15.0"
+  @alpine_version "3.16.2"
+  @elixir_version "1.14.1"
+  @erlang_version "25.1.2"
+  @node_asdf_version "18.12.1"
 
   defstruct base_module: nil,
             base_path: nil,
