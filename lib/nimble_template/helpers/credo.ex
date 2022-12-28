f0c8d9427887822188a4c5d3bbe06edef65d06fc
Thanh Huynh <huynhbathanh@gmail.com>
2022-12-28 09:03:39 +0700

    [gh-307] Refactor the template.ex structure

--- lib/nimble_template/helpers/credo.ex
+++ lib/nimble_template/helpers/credo.ex
@@ -1,10 +1,75 @@
 defmodule NimbleTemplate.CredoHelper do
   alias NimbleTemplate.Generator
+  alias NimbleTemplate.Projects.Project
+
+  @do_single_expression_rule_name "CompassCredoPlugin.Check.DoSingleExpression"
+  @single_module_file_rule_name "CompassCredoPlugin.Check.SingleModuleFile"
 
   @spec disable_rule(String.t(), String.t()) :: :ok | {:error, :failed_to_read_file}
   def disable_rule(file_path, rule) do
     Generator.prepend_content(file_path, """
     # credo:disable-for-this-file #{rule}
     """)
+  end
+
+  @spec disable_do_single_expression_rule(list()) :: :ok | {:error, :failed_to_read_file}
+  def disable_do_single_expression_rule(file_paths) do
+    Enum.each(file_paths, fn file_path ->
+      disable_rule(file_path, @do_single_expression_rule_name)
+    end)
+  end
+
+  @spec disable_single_module_file_rule(list()) :: :ok | {:error, :failed_to_read_file}
+  def disable_single_module_file_rule(file_paths) do
+    Enum.each(file_paths, fn file_path ->
+      disable_rule(file_path, @single_module_file_rule_name)
+    end)
+  end
+
+  def suppress_credo_warnings_for_base_project(%Project{base_module: base_module}) do
+    disable_do_single_expression_rule(["lib/#{Macro.underscore(base_module)}.ex"])
+  end
+
+  def suppress_credo_warnings_for_phoenix_project(
+        %Project{
+          base_path: base_path,
+          web_path: web_path
+        } = project
+      ) do
+    suppress_credo_warnings_for_base_project(project)
+
+    single_expression_files = [
+      "#{web_path}/controllers/page_controller.ex",
+      "#{web_path}/telemetry.ex",
+      "#{web_path}/views/error_view.ex",
+      "#{base_path}/release_tasks.ex"
+    ]
+
+    disable_do_single_expression_rule(single_expression_files)
+  end
+
+  def suppress_credo_warnings_for_phoenix_api_project(
+        %Project{
+          web_test_path: web_test_path,
+          base_path: base_path,
+          web_path: web_path
+        } = project
+      ) do
+    suppress_credo_warnings_for_base_project(project)
+
+    single_module_files = [
+      "#{web_test_path}/views/api/error_view_test.exs",
+      "#{web_test_path}/params/params_validator_test.exs"
+    ]
+
+    disable_single_module_file_rule(single_module_files)
+
+    single_expression_files = [
+      "#{base_path}/release_tasks.ex",
+      "#{web_path}/telemetry.ex",
+      "#{web_path}/views/error_view.ex"
+    ]
+
+    disable_do_single_expression_rule(single_expression_files)
   end
 end
--- lib/nimble_template/templates/template.ex
+++ lib/nimble_template/templates/template.ex
@@ -2,9 +2,9 @@ defmodule NimbleTemplate.Templates.Template do
   @moduledoc false
 
   import NimbleTemplate.DependencyHelper
+  import NimbleTemplate.CredoHelper
 
   alias NimbleTemplate.Addons.ExUnit
-  alias NimbleTemplate.CredoHelper
   alias NimbleTemplate.Projects.Project
   alias NimbleTemplate.Templates.Mix.Template, as: MixTemplate
   alias NimbleTemplate.Templates.Phoenix.Template, as: PhoenixTemplate
@@ -31,80 +31,33 @@ defmodule NimbleTemplate.Templates.Template do
 
   defp post_apply!(%Project{mix_project?: true} = project) do
     order_dependencies!()
-    fetch_and_install_elixir_dependencies()
+    install_elixir_dependencies()
     suppress_credo_warnings_for_base_project(project)
     format_codebase()
   end
 
   defp post_apply!(%Project{api_project?: true} = project) do
     order_dependencies!()
-    fetch_and_install_elixir_dependencies()
+    install_elixir_dependencies()
     suppress_credo_warnings_for_phoenix_api_project(project)
     format_codebase()
   end
 
   defp post_apply!(%Project{web_project?: true} = project) do
     order_dependencies!()
-    fetch_and_install_elixir_dependencies()
-    fetch_and_install_node_dependencies()
+    install_elixir_dependencies()
+    install_node_dependencies()
     suppress_credo_warnings_for_phoenix_project(project)
     format_codebase()
   end
 
-  defp fetch_and_install_elixir_dependencies() do
+  defp install_elixir_dependencies() do
     Mix.shell().cmd("MIX_ENV=dev mix do deps.get, deps.compile")
     Mix.shell().cmd("MIX_ENV=test mix do deps.get, deps.compile")
   end
 
-  defp fetch_and_install_node_dependencies() do
+  defp install_node_dependencies() do
     Mix.shell().cmd("npm install --prefix assets")
-  end
-
-  defp suppress_credo_warnings_for_base_project(%Project{base_module: base_module}) do
-    CredoHelper.disable_rule(
-      "lib/#{Macro.underscore(base_module)}.ex",
-      "CompassCredoPlugin.Check.DoSingleExpression"
-    )
-  end
-
-  defp suppress_credo_warnings_for_phoenix_project(
-         %Project{
-           base_path: base_path,
-           web_path: web_path
-         } = project
-       ) do
-    suppress_credo_warnings_for_base_project(project)
-
-    Enum.each(
-      [
-        "#{web_path}/controllers/page_controller.ex",
-        "#{web_path}/telemetry.ex",
-        "#{web_path}/views/error_view.ex",
-        "#{base_path}/release_tasks.ex"
-      ],
-      fn file_path ->
-        CredoHelper.disable_rule(file_path, "CompassCredoPlugin.Check.DoSingleExpression")
-      end
-    )
-  end
-
-  defp suppress_credo_warnings_for_phoenix_api_project(
-         %Project{
-           web_test_path: web_test_path
-         } = project
-       ) do
-    suppress_credo_warnings_for_base_project(project)
-    suppress_credo_warnings_for_phoenix_project(project)
-
-    Enum.each(
-      [
-        "#{web_test_path}/views/api/error_view_test.exs",
-        "#{web_test_path}/params/params_validator_test.exs"
-      ],
-      fn file_path ->
-        CredoHelper.disable_rule(file_path, "CompassCredoPlugin.Check.SingleModuleFile")
-      end
-    )
   end
 
   defp format_codebase() do
