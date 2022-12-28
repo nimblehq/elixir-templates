abc238f3bfdbe2bcf5a2dca73c2ab9552569d3e0
Thanh Huynh <huynhbathanh@gmail.com>
2022-12-28 11:00:47 +0700

    [gh-307] Add checking file exist

--- lib/nimble_template/helpers/credo.ex
+++ lib/nimble_template/helpers/credo.ex
@@ -7,9 +7,11 @@ defmodule NimbleTemplate.CredoHelper do
 
   @spec disable_rule(String.t(), String.t()) :: :ok | {:error, :failed_to_read_file}
   def disable_rule(file_path, rule) do
-    Generator.prepend_content(file_path, """
-    # credo:disable-for-this-file #{rule}
-    """)
+    if File.exists?(file_path) do
+      Generator.prepend_content(file_path, """
+      # credo:disable-for-this-file #{rule}
+      """)
+    end
   end
 
   @spec disable_do_single_expression_rule(list()) :: :ok | {:error, :failed_to_read_file}
@@ -38,38 +40,39 @@ defmodule NimbleTemplate.CredoHelper do
       ) do
     suppress_credo_warnings_for_base_project(project)
 
-    single_expression_files = [
+    files_contain_single_expression = [
       "#{web_path}/controllers/page_controller.ex",
       "#{web_path}/telemetry.ex",
       "#{web_path}/views/error_view.ex",
       "#{base_path}/release_tasks.ex"
     ]
 
-    disable_do_single_expression_rule(single_expression_files)
+    disable_do_single_expression_rule(files_contain_single_expression)
   end
 
   def suppress_credo_warnings_for_phoenix_api_project(
         %Project{
-          web_test_path: web_test_path,
           base_path: base_path,
-          web_path: web_path
+          web_path: web_path,
+          web_test_path: web_test_path
         } = project
       ) do
     suppress_credo_warnings_for_base_project(project)
 
-    single_module_files = [
+    files_contain_multiple_module = [
       "#{web_test_path}/views/api/error_view_test.exs",
       "#{web_test_path}/params/params_validator_test.exs"
     ]
 
-    disable_single_module_file_rule(single_module_files)
+    disable_single_module_file_rule(files_contain_multiple_module)
 
-    single_expression_files = [
+    files_contain_single_expression = [
+      "#{web_path}/controllers/page_controller.ex",
       "#{base_path}/release_tasks.ex",
       "#{web_path}/telemetry.ex",
       "#{web_path}/views/error_view.ex"
     ]
 
-    disable_do_single_expression_rule(single_expression_files)
+    disable_do_single_expression_rule(files_contain_single_expression)
   end
 end
