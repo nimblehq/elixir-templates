8fafd235f481bce285b2cb83ac0e654b9ca03843
Andy Duong <andyduong1920@gmail.com>
2023-03-10 15:14:17 +0700

    [gh315] Fix the gettext

--- lib/nimble_template/templates/variants/phoenix/template.ex
+++ lib/nimble_template/templates/variants/phoenix/template.ex
@@ -45,9 +45,6 @@ defmodule NimbleTemplate.Templates.Phoenix.Template do
   end
 
   defp apply_phoenix_common_setup(%Project{} = project) do
-    # TODO: Remove me after the Phoenix generator fix releases: https://github.com/phoenixframework/phoenix/pull/4894
-    remove_mix_compiler_config!()
-
     project
     |> apply_default_common_phoenix_addons()
     |> apply_optional_common_phoenix_addons()
@@ -142,7 +139,4 @@ defmodule NimbleTemplate.Templates.Phoenix.Template do
 
   defp apply_phoenix_variant_setup(%Project{web_project?: true, live_project?: true} = project),
     do: LiveTemplate.apply!(project)
-
-  defp remove_mix_compiler_config!(),
-    do: Generator.delete_content!("mix.exs", "compilers: [:gettext] ++ Mix.compilers(),")
 end
